2a9cb0a64b71c5d43878085a43723e34fa0a831e
Jonathan Huston <jonathan.huston@gmail.com>
2020-10-04 14:11:04 +0200

    separate plain-text ns

---
+++ src/slatescript_tools/plain_text.clj
@@ -0,0 +1,47 @@
+(ns slatescript-tools.plain-text
+  (:require [clojure.string :as str]
+            [clojure.xml :as xml]))
+
+(def breaking-tags #{:w:p :w:br})
+(def ignore-tags #{:w:sectPr :w:rFonts :w:color :w:w :w:sz})
+
+(declare get-content)
+
+(defn get-content-from-vector
+  "iterates through content vector, eliminates multiple spaces"
+  [tag contents]
+  (let [clean (->
+               (reduce #(str %1 (get-content %2)) "" contents)
+               (str/replace #" +" " ")
+               (str/replace #"\n " "\n"))]
+    (if (some #(= tag %) breaking-tags)
+      (str clean "\n")
+      clean)))
+
+(defn space?
+  "add special space?"
+  [tag attrs contents]
+  (and
+   (nil? contents)
+   (or (= tag :w:spacing) (= attrs {:xml:space "preserve"}))))
+
+(defn get-content
+  "gets content from xml"
+  [xml]
+  (let [tag (:tag xml)
+        attrs (:attrs xml)
+        contents (:content xml)
+        first-content (first contents)]
+    (cond
+      (some #(= tag %) ignore-tags) ""
+      (string? first-content) first-content
+      (space? tag attrs contents) " "
+      :else (get-content-from-vector tag contents))))
+
+(defn plain-text
+  "gets plain text from document-xml"
+  [document-xml]
+  (->
+   document-xml
+   xml/parse
+   get-content))
