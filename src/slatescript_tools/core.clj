1508a16a8b427d0210492ad4d3d4e6d25f3d2e77
Jonathan Huston <jonathan.huston@gmail.com>
2020-10-18 21:05:27 +0200

    CLI args validation

--- src/slatescript_tools/core.clj
+++ src/slatescript_tools/core.clj
@@ -1,23 +1,9 @@
 (ns slatescript-tools.core
   (:gen-class)
-  (:require [slatescript-tools.plain-text :refer [plain-text]]
-            [slatescript-tools.shell :refer [trim-ext create-xml remove-xml create-docx]]
+  (:require [clojure.java.io :as io]
+            [slatescript-tools.plain-text :refer [plain-text]]
+            [slatescript-tools.shell :refer [trim-ext create-xml remove-xml]]
             [slatescript-tools.validate :refer [parens checknums]]))
-
-
-; DEV
-(defn- parse-xml
-  "parse xml file only"
-  [xml-file]
-  (clojure.xml/parse xml-file))
-
-; DEV
-(defn- display-plain-text
-  "given xml file, display plain text"
-  [xml-file]
-  (->
-   xml-file
-   plain-text))
 
 
 (defn save-as-txt
@@ -51,14 +37,38 @@
     (remove-xml docx2)
     result))
 
+(defn- validate-args
+  "validates command line arguments"
+  [tool args num]
+  (let [arity (count args)
+        sing-plur (if (= num 1) "argument," "arguments,")]
+    (cond 
+      (not= arity num) (println "Should be" num sing-plur arity "passed.")
+      (not-every? true? (map #(.exists (io/file %)) args)) (println "File not found.")
+      :else (apply tool args))))
+
+(defn- print-help-text 
+  "print CLI help text"
+  []
+  (println 
+"Tools for validating translated Word documents
+
+Usage:
+   slatescript-tools <tool> docx1 [docx2]
+            
+Tools:
+   save-as-txt docx1:          converts docx1 to UTF-8 text
+   check-parens docx1:         checks whether parentheses are balanced
+   check-numbers docx1 docx2:  checks whether digits are the same in docx1 and docx2"))
+
 (defn -main 
   "launches save-as-txt, check-parens, and check-numbers tools"
   ([]
    (-main "help"))
   ([tool & args]
    (case tool
-     "save-as-txt" (save-as-txt (first args))
-     "check-parens" (prn (check-parens (first args)))
-     "check-numbers" (run! prn (check-numbers (first args) (second args)))
-     (println "Usage: slatescript-tools <save-as-txt|check-parens|check-numbers> docx1 [docx2]"))
+     "save-as-txt" (validate-args save-as-txt args 1)
+     "check-parens" (run! prn (validate-args check-parens args 1))
+     "check-numbers" (run! prn (validate-args check-numbers args 2))
+     (print-help-text))
    (shutdown-agents)))
